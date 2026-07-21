cf4e652adda236e95229ff1ad72e2956dfa12591
dev-tarun <tarun.yadav42009@gmail.com>
2026-07-21 17:20:43 +0530

    :wq

--- demoabstract.java
+++ demoabstract.java
@@ -1,7 +1,14 @@
+/**
+ * Represents a base abstraction layer demonstrating abstract class design.
+ * This class provides a mix of fully implemented and template methods
+ * to define common behaviors and enforce architectural contracts.
+ */
 abstract class demoabstract {
-public void test(){
-    System.out.println("this is a body of class");
-}
- abstract public void pest();
- abstract   public void copy();   
+    public void test() {
+        System.out.println("this is a body of class");
+    }
+
+    abstract public void pest();
+
+    abstract public void copy();
 }
--- out.java
+++ out.java
@@ -1,18 +1,22 @@
+/**
+ * Concrete implementation of the {@link demoabstract} base class.
+ * This class fulfills the architectural contract by providing specific 
+ * implementations for the inherited abstract methods and serves as the execution entry point.
+ */
+
 public class out extends demoabstract {
-    public void pest(){
+    public void pest() {
         System.out.println("this is pest of demoabstract ");
     }
-    public void copy(){
+
+    public void copy() {
         System.out.println("This is copy of demoastract ");
     }
 
     public static void main(String[] args) {
-        out o1= new out();
+        out o1 = new out();
         o1.copy();
-
-
-        
         o1.pest();
-        o1.test();    
+        o1.test();
     }
 }
